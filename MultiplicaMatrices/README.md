534e7f125083e7482b4a5581df166896e4b4ec2c
MiguelMontealegre <miguelmontealegre808@gmail.com>
2026-02-22 15:23:12 -0500

    multi-theread

--- MultiplicaMatrices/README.md
+++ MultiplicaMatrices/README.md
@@ -1,2 +1,2 @@
-gcc pr.c /programa
-for i in 100 200 400 600 800 1000 2000; do ./programa $i >>times2.doc; done; done
+gcc -o programa prueba1.c
+for i in 100 200 400 600 800 1000 2000; do ./programa $i >> times2.doc; done
--- MultiplicaMatrices/ejecutar_pruebas.sh
+++ MultiplicaMatrices/ejecutar_pruebas.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Compilar el programa en esta máquina (evita "cannot execute binary file")
+echo "Compilando multi_secuencial.c..."
+gcc -o multi_secuencial multi_secuencial.c -O2
+if [ $? -ne 0 ]; then
+    echo "Error al compilar. Abortando."
+    exit 1
+fi
+
 echo "[" > resultados.json
 
 for i in 500 1000 2000 4000 6000 8000
@@ -8,7 +16,12 @@ do
     ./multi_secuencial $i >> resultados.json
 done
 
-# Esto es un pequeño truco para quitar la última coma sobrante 
-# y cerrar el arreglo de JSON
-sed -i '$ s/,$//' resultados.json
+# Quitar la última coma sobrante y cerrar el arreglo JSON (compatible con macOS y Linux)
+if sed --version 2>/dev/null | grep -q GNU; then
+    sed -i '$ s/,$//' resultados.json
+else
+    sed -i '' '$ s/,$//' resultados.json
+fi
 echo "]" >> resultados.json
+
+echo "Listo. Resultados en resultados.json"
